0192e324fbd0c6bf9d14cc4133fdafa990ccdf1b
Atharv <athi.aug31@gmail.com>
2023-04-24 02:39:27 +0530

    first commit

---
+++ Addition.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Addition{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        // Ask user to input two numbers
+        System.out.print("Enter the first number: ");
+        int num1 = input.nextInt();
+        
+        System.out.print("Enter the second number: ");
+        int num2 = input.nextInt();
+        
+        // Add the two numbers and display the result
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
+    }
+}
